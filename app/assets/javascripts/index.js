d3bfa1f6c0d74581af4ba186a0d2987fa79d4b34
secondwtq <lovejay-lovemusic@outlook.com>
2016-03-09 21:32:33 +0800

    Frontend details.

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -2,21 +2,4 @@ $(function () {
     $('.nav-bar-profile-show-btn').on('click', function () {
         $('.nav-bar-profile-menu-cont').toggleClass('nav-bar-profile-menu-active');
     });
-    
-    var seatCount = 0;
-    var MAX_SEAT_COUNT = 4;
-    $('.seat > input[type="checkbox"]').change(function (e) {
-        if (e.target.checked == true) {
-            seatCount++;
-        } else {
-            seatCount--;
-        }
-        if (seatCount >= MAX_SEAT_COUNT) {
-            $('.seat > input[type="checkbox"]:not(:checked)').addClass('seat-disabled');
-        } else {
-            $('.seat > input[type="checkbox"]').removeClass('seat-disabled');
-        }
-        
-        $('.seat-count').text(seatCount.toString());
-    });
 });
