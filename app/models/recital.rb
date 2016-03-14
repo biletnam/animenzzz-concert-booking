74972debbb961fd6abaf5d39e4541efd9f186d6d
Windas <Wind_as@outlook.com>
2016-03-14 11:00:19 +0800

    Use dynamic image resource in recital model

--- app/models/recital.rb
+++ app/models/recital.rb
@@ -1,10 +1,11 @@
 class Recital < ActiveRecord::Base
-  has_attached_file :poster, default_url: '/images/recitals/poster'
+  has_attached_file :poster, default_url: 'poster.png'
   validates_attachment_content_type :poster, content_type: /\Aimage/
   validates_attachment_file_name :poster, matches: [/png\Z/, /jpe?g\Z/]
 
-  has_attached_file :venue_image, default_url: '/images/recitals/:venue_image'
+  has_attached_file :venue_image, default_url: 'area-map.jpg'
   validates_attachment_content_type :venue_image, content_type: /\Aimage/
   validates_attachment_file_name :venue_image, matches: [/png\Z/, /jpe?g\Z/]
+
   has_many :areas, dependent: :destroy
 end
