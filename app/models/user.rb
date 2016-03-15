357a88bc9027498bdc7764e37c5ae0528c90ad61
Windas <Wind_as@outlook.com>
2016-03-15 16:42:45 +0800

    Add avatar to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,13 @@ class User < ActiveRecord::Base
   enum role: [:user, :vip, :admin]
   after_initialize :set_default_role, :if => :new_record?
 
+  has_attached_file :avatar,
+                    styles: { large: "300x300>", medium: "100x100>", thumb: "40x40>" },
+                    default_url: "default-thumb.png",
+                    url: '/attachments/:class/:id/:attachment/:style/:filename',
+                    path: ':rails_root/public/attachments/:class/:id/:attachment/:style/:filename'
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+
   def set_default_role
     self.role ||= :user
   end
