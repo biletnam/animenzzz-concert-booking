dbe8743af249318d369f92cd04e07918ed0f8503
Windas <Wind_as@outlook.com>
2016-03-15 10:46:42 +0800

    Add recital status

--- app/models/recital.rb
+++ app/models/recital.rb
@@ -8,4 +8,14 @@ class Recital < ActiveRecord::Base
   validates_attachment_file_name :venue_image, matches: [/png\Z/, /jpe?g\Z/]
 
   has_many :areas, dependent: :destroy
+
+
+  def sold_out?
+    self.areas.each do |area|
+  	  area.seats.each do |seat|
+  		return false unless seat.sold
+  	  end
+  	end
+  	true
+  end
 end
