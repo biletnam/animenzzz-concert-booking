e1f28dc09e2b0fcd70100000266518ad0698cbb1
Windas <Wind_as@outlook.com>
2016-03-24 13:02:44 +0800

    Update video store path

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,8 +2,8 @@ class Video < ActiveRecord::Base
   has_attached_file :screenshot, 
   					default_url: 'video_areuok.jpg',
   					styles: { large: "600x340>", medium: "300x170>" },
-  					url: '/attachments/:class/:id/:attachment/:filename',
-  					path: ':rails_root/public/attachments/:class/:id/:attachment/:filename'
+  					url: '/attachments/:class/:id/:attachment/:style/:filename',
+  					path: ':rails_root/public/attachments/:class/:id/:attachment/:style/:filename'
   validates_attachment_content_type :screenshot, content_type: /\Aimage/
   validates_attachment_file_name :screenshot, matches: [/png\Z/, /jpe?g\Z/]
 
