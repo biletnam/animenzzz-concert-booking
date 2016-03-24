69eec7505bebd805a25ea880b0daf4303555df8c
Windas <Wind_as@outlook.com>
2016-03-24 13:32:12 +0800

    Use indexVideo insead of titleVideo

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,7 +2,7 @@ class VisitorsController < ApplicationController
   def index
   	@recitals = Recital.all.sort_by {|r| r.id }
   	videos = Video.arel_table
-  	@titleVideo = Video.where(videos[:name].eq('Unravel (Tokyo Ghoul OP1)')).first
+  	@indexVideo = Video.where(videos[:name].eq('Unravel (Tokyo Ghoul OP1)')).first
   	@videos = Video.where(videos[:name].not_eq('Unravel (Tokyo Ghoul OP1)'))
   end
 end
