a6ccc29ef2ecfcd6f8043efe957ab66190e6b2d2
Windas <Wind_as@outlook.com>
2016-03-27 01:35:50 +0800

    Order ASC

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,7 +2,7 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @orders = current_user.orders(order: 'created_at DESC')
+    @orders = current_user.orders(order: 'created_at ASC')
   end
 
   def show
