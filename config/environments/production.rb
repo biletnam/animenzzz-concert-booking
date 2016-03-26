dec52b53722c5ed43d08284cbf7b234d13fc9433
Windas <Wind_as@outlook.com>
2016-03-26 15:16:44 +0800

    Change mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,13 +72,14 @@ Rails.application.configure do
   config.active_support.deprecation = :notify
 
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: Rails.application.secrets.domain_name,
+    address: "smtp.exmail.qq.com",
+    port: 465,
+    domain: 'qq.com',
     authentication: "plain",
     enable_starttls_auto: true,
-    user_name: Rails.application.secrets.email_provider_username,
-    password: Rails.application.secrets.email_provider_password
+    user_name: 'admin@yuxianglishun.cn',
+    password: 'Yuyang123.mail'
+    ssl: true
   }
   # ActionMailer Config
   config.action_mailer.default_url_options = { :host => Rails.application.secrets.domain_name }
