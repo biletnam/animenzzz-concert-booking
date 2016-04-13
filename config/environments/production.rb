25e8ef59548b3f8981274baa24bf737597cc7569
secondwtq <lovejay-lovemusic@outlook.com>
2016-04-14 00:26:42 +0800

    CDN in production.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.action_controller.asset_host = 'http://assets.example.com'
+  config.action_controller.asset_host = 'http://image.yuxianglishun.cn'
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
@@ -95,7 +95,6 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   Paperclip.options[:command_path] = "/usr/bin/"
-
 
   config.react.variant = :production
   config.react.addons = true
