bc9661c19b95b119dddd9b745378477dbc4d8d7c
Windas <Wind_as@outlook.com>
2016-05-31 15:21:42 +0800

    Update area controller

--- app/controllers/areas_controller.rb
+++ app/controllers/areas_controller.rb
@@ -38,6 +38,7 @@ class AreasController < ApplicationController
       	  next if area[:type] != 'seat'
           a = Area.where(:klass => area[:area]).first
           next if a.name != 'C区' and a.name != 'D区' and a.name != 'K区'
+          next if a.joins(:seats).where(seats: {locate_x: area[:row], locate_y: area[:num]}).size != 0
       	  seat = Seat.create(locate_x: area[:row], locate_y: area[:num])
       	  price = Price.where(:price => area[:price]).first
       	  price.seats << seat
